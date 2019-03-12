75e0c2b7617f2cba198d706f5eaa44c80ac28285
Jose Fernandez-Castano <josefernandezcastano@gmail.com>
2019-03-12 18:57:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+   drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name){
+  drivers.pop(name)
+}
+
+function destructivelyRemoveFirstDriver(name){
+  drivers.shift(name)
+}
+
+function appendDriver(name){
+  const copyAppendDriver = [...drivers, name]
+  return copyAppendDriver;
+}
+
